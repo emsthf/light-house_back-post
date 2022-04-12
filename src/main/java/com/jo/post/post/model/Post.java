3e954f282a866d4170e2c6e539e385f290919540
emsthf <deunsol916@gmail.com>
2022-04-12 18:56:43 +0900

    edit post image upload

--- src/main/java/com/jo/post/post/model/Post.java
+++ src/main/java/com/jo/post/post/model/Post.java
@@ -14,18 +14,14 @@ public class Post {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-<<<<<<< HEAD
-    @ManyToOne
-    @JoinColumn(name = "category_id")
-    private CategoryEntity category;
-=======
+
     @Enumerated(EnumType.STRING) @Column(nullable = false)
     private Category category;
->>>>>>> f1979ddfe81133f1ece23984e6ddbce5c10a2ed9
     @Column(nullable = false)
     private String title;
     @Column(nullable = false)
     private String content;
+    @Column(nullable = false)
     private String postImg;
     private LocalDate created;
     private Long goalId;
