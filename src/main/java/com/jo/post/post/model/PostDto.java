eb042d97064fb9d4e81532a96d50c013e1e5f370
rivera00255 <rivera00255@gmail.com>
2022-04-12 17:42:51 +0900

    arrange

---
+++ src/main/java/com/jo/post/post/model/PostDto.java
@@ -0,0 +1,43 @@
+package com.jo.post.post.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class PostDto {
+
+    private Long id;
+    private Long categoryId;
+    private String title;
+    private String content;
+    private String postImg;
+    private LocalDate created;
+    private Long goalId;
+    private Long userId;
+
+    public PostDto(Long id, Long categoryId, String title, String content, String postImg, LocalDate created, Long goalId, Long userId) {
+        this.id = id;
+        this.categoryId = categoryId;
+        this.title = title;
+        this.content = content;
+        this.postImg = postImg;
+        this.created = created;
+        this.goalId = goalId;
+        this.userId = userId;
+    }
+
+    public PostDto(Long id, Long categoryId, String title, String content, String postImg, LocalDate created, Long goalId) {
+        this.id = id;
+        this.categoryId = categoryId;
+        this.title = title;
+        this.content = content;
+        this.postImg = postImg;
+        this.created = created;
+        this.goalId = goalId;
+    }
+}
---
+++ src/main/java/com/jo/post/post/service/PostService.java
@@ -0,0 +1,20 @@
+package com.jo.post.post.service;
+
+import com.jo.post.post.model.Post;
+import com.jo.post.post.model.PostDto;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface PostService {
+
+    Long savePost(PostDto postDto);
+    List<Post> findAllPost();
+    Optional<Post> findById(Long id);
+    void editPost(Long id, PostDto postDto);
+    void delPost(Long id);
+    List<Post> findAllByGoalId(Long goalId);
+    Optional<Post> findByGoalIdAndCreated(Long goalId, LocalDate created);
+    void deleteAll(Long userId, Long GoalId);
+}
